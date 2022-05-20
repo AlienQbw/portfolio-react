db0958b4c9162d21f951ab49fb606d3f95bb9b25
AlienQbw <wladyka.bj@gmail.com>
2022-05-20 17:19:02 +0200

    Created courses website template with grid placement randomizer

--- src/views/projects/courses/Courses.js
+++ src/views/projects/courses/Courses.js
@@ -1,6 +1,82 @@
+import { useEffect, useState } from 'react';
 import { Wrapper } from './Courses.styles';
+import HelloRomanReactThumbnail from 'assets/resources/courses/helloRomanReact.jpg';
+import JonasSchmedtmannJavaScriptThumbnail from 'assets/resources/courses/jonasSchmedtmannJavaScript.jpg';
 
 const Courses = () => {
-  return <Wrapper>Courses component</Wrapper>;
+  const [coursesList, setCoursesList] = useState([]);
+  const data = [
+    {
+      name: 'Hello Roman React Course',
+      thumbnail: HelloRomanReactThumbnail,
+    },
+    {
+      name: 'Jonas Schmedtmann - Javascript Course',
+      thumbnail: JonasSchmedtmannJavaScriptThumbnail,
+    },
+    {
+      name: 'Hello Roman React Course',
+      thumbnail: HelloRomanReactThumbnail,
+    },
+    {
+      name: 'Hello Roman React Course',
+      thumbnail: HelloRomanReactThumbnail,
+    },
+    {
+      name: 'Hello Roman React Course',
+      thumbnail: HelloRomanReactThumbnail,
+    },
+    {
+      name: 'Hello Roman React Course',
+      thumbnail: HelloRomanReactThumbnail,
+    },
+  ];
+
+  const checkColumnNumber = (id) => {
+    console.log(id);
+    if (id === 0) {
+      return id + 1;
+    }
+    if (id * 3 < 4 && id !== 0) {
+      return id * 3 + 1;
+    }
+    let columnNumber = id * 3;
+    while (columnNumber > 4) {
+      columnNumber -= 4;
+      if (columnNumber === 0) {
+        return columnNumber++;
+      }
+    }
+
+    return columnNumber;
+  };
+
+  useEffect(() => {
+    setCoursesList(...coursesList, data);
+  }, []);
+
+  console.log(coursesList);
+
+  return (
+    <Wrapper>
+      <h2>Courses from which I've learnt!</h2>
+      <div className="grid-container">
+        {coursesList.map((item, id) => {
+          return (
+            <div
+              style={{
+                gridColumnStart: `${checkColumnNumber(id)}`,
+              }}
+              className="grid-item"
+              key={id}
+            >
+              <h3>{item.name}</h3>
+              <img src={item.thumbnail} alt="" />
+            </div>
+          );
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 export default Courses;
--- src/views/projects/courses/Courses.styles.js
+++ src/views/projects/courses/Courses.styles.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  .grid-container {
+    padding: 4%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: auto 1fr auto;
+    grid-gap: 5px;
+    .grid-item {
+      img {
+        aspect-ratio: 16/9;
+        width: 100%;
+      }
+    }
+  }
+`;
