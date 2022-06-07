3115cb09383ac385f57bda8904f59d1ac2d6f14e
AlienQBw <wladyka.bj@gmail.com>
2022-06-07 02:53:39 +0200

    initial code, to introduce onScroll behavior

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,19 +1,26 @@
 import { Wrapper } from './Home.styles';
 import HomeImage from '../../assets/svg/home-image.svg';
 import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
+  /* TODO: learn how to use Navigate */
+  const navigate = useNavigate();
+  /* TODO: learn how to get current location.pathname of the entire application */
   const intervalRef = useRef();
   const titleText = 'Frontend Developer';
   const [title, setTitle] = useState('F');
   if (title.length === titleText.length) {
     clearInterval(intervalRef.current);
   }
+
   useEffect(() => {
     intervalRef.current = setInterval(
       () => setTitle((title) => title + titleText[title.length]),
       75
     );
-
+    window.addEventListener('wheel', (e) => {
+      e.deltaY < 0 ? console.log(`up`) : console.log(`down`);
+    });
     return () => {
       clearInterval(intervalRef.current);
     };
